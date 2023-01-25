ec1992367a24b7dd7aab1f0a15c16df6410d36ce
Mareks1993 <marekstaszkiewicz@live.com>
2023-01-24 21:52:10 -0500

    added function passMatch which checks if both password inputs match and change a boolean which applies a error class if 0

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 let stringPass = "";
 let stringPass2 = "";
-
-function stringBuilder(string,char){
-    if (char.length == 1){
-        return string+char;
-    }else{
-        return stringPass;
-    }
+let passwordMatch = 0;
+// function stringBuilder(string,char){
+//     if (char.length == 1){
+//         return string+char;
+//     }else{
+//         return stringPass;
+//     }
    
-}
+// }
+function passMatch(){
+    if(pass.value == pass2.value){
+        passwordMatch = 1;
+    }
+    else{
+        passwordMatch = 0;
+    }
 
+    if(passwordMatch == 0){
+        pass.classList.add('error');
+        pass2.classList.add('error');
+    }
+    else{
+        pass.classList.remove('error');
+        pass2.classList.remove('error');
+    }
+}
 const pass = document.querySelector('#password');
 const pass2 = document.querySelector('#confirm_password');
 
     pass.addEventListener('keyup',(e)=>{
-        // console.log(e);
-        // console.log(e.key);
+
         console.log(pass.value);
-        stringPass = stringBuilder(stringPass,e.key);
-        console.log("stringPass is "+ stringPass);
+        // stringPass = stringBuilder(stringPass,e.key);
+        passMatch()
     });
 
     pass2.addEventListener('keyup',(e)=>{
         console.log(e);
+        passMatch();
     });
